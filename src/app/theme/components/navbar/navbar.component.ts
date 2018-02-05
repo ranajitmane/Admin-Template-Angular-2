47db6c6431ba6bb3224edb778e88a20d70de2463
Ranajit Mane <maneranajit@gmail.com>
2018-02-06 00:55:24 +0530

    Updated from local
    Updated from local

---
+++ src/app/theme/components/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { AppState } from '../../../app.state';
+import { SidebarService } from '../sidebar/sidebar.service';
+
+@Component({
+  selector: 'az-navbar',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  providers: [ SidebarService ]
+})
+
+export class NavbarComponent {
+    public isMenuCollapsed:boolean = false;
+
+    constructor(private _state:AppState, private _sidebarService:SidebarService) {
+        this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
+            this.isMenuCollapsed = isCollapsed;
+        });
+    }
+
+    public closeSubMenus(){
+       this._sidebarService.closeAllSubMenus();
+    }
+
+    public toggleMenu() {
+        this.isMenuCollapsed = !this.isMenuCollapsed; 
+        this._state.notifyDataChanged('menu.isCollapsed', this.isMenuCollapsed);        
+    }
+
+}
